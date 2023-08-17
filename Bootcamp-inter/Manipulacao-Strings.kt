5389b76047810c0f1518db181e8b38cdac66f266
francovinicius <duartevini.dev@gmail.com>
2023-08-17 10:51:54 -0300

    add manipulacao de strings

---
+++ Bootcamp-inter/Manipulacao-Strings.kt
@@ -0,0 +1,32 @@
+/*
+* first() - 1 Posição
+* last() - Ultima posição
+* string.length
+* string[index] */
+
+fun main () {
+    val welcome = "Bem vindo(a)"
+    var name = "ana"
+
+    println("$welcome, ${name.capitalize()}!")
+}
+
+/*
+* Capitalização de strings
+*
+* capitalize(), toUpperCase(), toLowerCase(), decapitalize()
+*
+* Remocao de espacos
+*
+* trimEnd(), trimStart(), tim()
+*
+* Substituicao de caracteres
+*
+* replace(x,y)
+*
+* Formatacao
+*
+* "padrao ${valor}.format(valor)*/
+
+
+//Empty X Blank
