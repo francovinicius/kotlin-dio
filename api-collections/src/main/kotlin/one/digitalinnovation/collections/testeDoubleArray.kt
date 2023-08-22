54e02080d4982f2b52b897ded3fc3ee4f67a8e06
francovinicius <duartevini.dev@gmail.com>
2023-08-22 11:53:03 -0300

    add double array

--- api-collections/src/main/kotlin/one/digitalinnovation/collections/testeDoubleArray.kt
+++ api-collections/src/main/kotlin/one/digitalinnovation/collections/testeDoubleArray.kt
@@ -1,2 +1,27 @@
 package one.digitalinnovation.collections
 
+fun main() {
+    val salarios = DoubleArray(3)
+    salarios[0] = 1000.0
+    salarios[1] = 100.0
+    salarios[2] = 300.0
+
+    salarios.forEach { println(it) }
+    println("----------")
+
+    //dar um aumento de 10%
+    salarios.forEachIndexed { index, salario ->
+        salarios[index] = salario * 1.1
+    }
+
+    salarios.forEach { println(it) }
+
+    println("----------")
+
+    val salarios2 = doubleArrayOf(1500.0, 150.0, 5000.0)
+    salarios2.sort()
+    salarios2.forEach {
+        println(it)
+    }
+
+}
