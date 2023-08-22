8452bb8eab63c5509b8555e9047332760c6275ea
francovinicius <duartevini.dev@gmail.com>
2023-08-22 12:30:19 -0300

    add count, find and any

--- api-collections/src/main/kotlin/one/digitalinnovation/collections/testeOperacoes.kt
+++ api-collections/src/main/kotlin/one/digitalinnovation/collections/testeOperacoes.kt
@@ -19,4 +19,17 @@ fun main() {
     //filtrando salarios maior que 2500
     val salariosMaioresQue2500 = salarios.filter { it > 2500.0 }
     salariosMaioresQue2500.forEach { println(it) }
+
+    println("------")
+    //realizar uma contagem percorrendo o array e vendo quantos elementos estão dentro do range abaixo
+    println(salarios.count{ it in 2000.00..5000.0})
+    println("------")
+
+    //realizar uma busca especifica
+    println(salarios.find{ it == 2250.0})
+    println(salarios.find{ it == 250.0})
+    println("------")
+
+    //encontrar qualquer
+    println(salarios.any{ it == 1000.0})
 }
